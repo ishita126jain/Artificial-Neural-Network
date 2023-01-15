ab0fe2b56c1f06e945ed9cdd4ba6b8ed93ed5e01
IshitaJain <91709949+ishita126jain@users.noreply.github.com>
2023-01-15 16:06:34 +0530

    Add files via upload

---
+++ p3.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+n = int(input("Enter number of neuron in hidden layer: "))
+ip = np.array([],dtype=int)
+
+size = int(input("Enter size of input array: "))
+for i in range (size):
+    x = int(input("Enter input: "))
+    ip = np.append(ip,x)
+print("Inputs are: ", ip)
+
+def percept(n,ip):
+    N = np.array([],dtype=int)
+    for i in range(n):
+        weight = np.random.randn(n)
+        print("weight are: ",weight)
+
+        ni = (ip*weight).sum()
+        N = np.append(N,ni)
+
+    print("Inputs for output layer neuron: ",N)
+    ow = np.random.randn(n)
+    print("Weights of output layer neuron are: ", ow)
+
+    op = (N*ow).sum()
+    return op
+
+op = percept(n,ip)
+print("Final output is: " ,op)
